57f542d789850678d1c24ffe8dd21596842d1a60
dewatriwijaya <dewatwc@gmail.com>
2024-12-29 20:14:34 +0700

    push change dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -68,7 +68,7 @@ def create_scatter_df(df):
 
 
 # Load cleaned data
-all_df = pd.read_csv("day.csv")
+all_df = pd.read_csv("dashboard/day.csv")
 # Mengubah data mnth menjadi kategori example: 1 -> January
 all_df['mnth'] = all_df['mnth'].map({1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'})
 
